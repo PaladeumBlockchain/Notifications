0ce4ed937cad7a33ef9164a37e618fcf0a1e00b3
volbil <volbilnexus@gmail.com>
2024-04-10 21:53:51 +0300

    Reenable sending notifications

--- sync.py
+++ sync.py
@@ -3,7 +3,7 @@ from service import sync
 
 background = BlockingScheduler(daemon=True, misfire_grace_time=None)
 
-# background.add_job(sync.send_notifications, "interval", minutes=1)
+background.add_job(sync.send_notifications, "interval", minutes=1)
 background.add_job(sync.check_addresses, "interval", minutes=1)
 
 background.start()
